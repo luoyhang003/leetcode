81fd25c8c60f21e89e4bd91c446dabbc5594d1e5
Jason Luo <luoyhang003@hotmail.com>
2017-03-22 12:47:12 +0800

    Initial Commit

---
+++ LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by 罗远航 on 22/03/2017.
+//  Copyright © 2017 罗远航. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
